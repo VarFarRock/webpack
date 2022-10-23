a9592800129613b1f0060391c006781bf9e74ad9
VarFarRock <vladrock31@gmail.com>
2022-10-23 20:13:57 +0300

    test2

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,4 @@
-import { create } from 'lodash';
+
 import './map.js'
 import './swiper__header.js'
 import './swiper__news.js'
@@ -8,23 +8,23 @@ let btn = document.querySelector('.hero__btn').onclick = () => {
 }
 //modal-img//
 // let img = document.querySelectorAll('.gallery__content-item');
-let popup = document.querySelector('.credits__popup')
-let out = document.querySelector('.credits__popup-contain')
-let wrap = document.querySelector('.wrapper')
-document.querySelector('.gallery__content-works').addEventListener('click', (e) => {
-    // if(e.target.className === 'gallery__content-item'){
-        console.log(e.target);
-        popup.classList.add('credits__popup--visible')
-        wrap.style.overflow = "hidden"
-    // }
-})
-// for(let i = 0;i < img.length;i++ ){
-//     img[i].addEventListener('target', () => {
-//             popup.classList.add('credits__popup--visible')
-//             wrap.style.overflow = "hidden"
-//     })
-// }
-document.querySelector('.credits__popup-close').addEventListener('click', (e) => {
-    document.querySelector('.credits__popup').classList.remove('credits__popup--visible')
-    document.querySelector('.wrapper').style.overflow = "visible"
-})
+// let popup = document.querySelector('.credits__popup')
+// let out = document.querySelector('.credits__popup-contain')
+// let wrap = document.querySelector('.wrapper')
+// document.querySelector('.gallery__content-works').addEventListener('click', (e) => {
+//     // if(e.target.className === 'gallery__content-item'){
+//         console.log(e.target);
+//         popup.classList.add('credits__popup--visible')
+//         wrap.style.overflow = "hidden"
+//     // }
+// })
+// // for(let i = 0;i < img.length;i++ ){
+// //     img[i].addEventListener('target', () => {
+// //             popup.classList.add('credits__popup--visible')
+// //             wrap.style.overflow = "hidden"
+// //     })
+// // }
+// document.querySelector('.credits__popup-close').addEventListener('click', (e) => {
+//     document.querySelector('.credits__popup').classList.remove('credits__popup--visible')
+//     document.querySelector('.wrapper').style.overflow = "visible"
+// })
